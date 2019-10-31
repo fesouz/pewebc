9f263e23aac629fa5911fa91f313c9d1c37f428d
2190773 <110803@Fef>
2019-10-31 22:48:28 +0000

    terminar

---
+++ F4/Ex3/F4_Ex3.js
@@ -0,0 +1,12 @@
+"user strict";
+
+var imagem = null;
+
+$(document).ready(function () {
+
+    imagem = $("img");
+
+    imagem.attr("width", "300");
+    imagem.attr("heigth", "170");
+    //imagem.height("170");
+})
